2b8c71285c97d4703eb66bd55b70e84303ac5dac
Samuel Noye <samuel.noye@amalitech.org>
2021-12-21 09:38:14 +0000

    Update ContentView.swift

--- CalculatorApp-IOS/ContentView.swift
+++ CalculatorApp-IOS/ContentView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - PROPERTIES
+    let buttons = [
+    ["AC","+","%","/"],
+    ["7","8","9","X"],
+    ["4","5","6","-"],
+    ["1","2","3","+"],
+    ["0","",".","="]
+    ]
+    
+    //MARK: - BODY
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Spacer()
+            ForEach(buttons, id: \.self){ row in
+                HStack{
+                    ForEach(row, id: \.self){ button in
+                        Text(button)
+                            .font(.system(size: 32))
+                            .frame(width: 70, height: 70)
+
+                    }
+                }
+            }
+        }
+        
     }
 }
 
+
+  //MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
