9ada321271d3646dbf905e432192b22eca86a0ea
Samuel Noye <samuel.noye@amalitech.org>
2021-12-21 09:02:08 +0000

    Initial Commit

---
+++ CalculatorApp-IOS/CalculatorApp_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp_IOSApp.swift
+//  CalculatorApp-IOS
+//
+//  Created by Samuel Noye on 21/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
